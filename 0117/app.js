29aab0028d1e735eb6f4eb470a1971ebec5b7bfb
gongbiao <developbiao@gmail.com>
2017-01-18 21:56:03 +0800

    learning ui-router nested lesson part01.

--- 0117/app.js
+++ 0117/app.js
@@ -1,2 +1,36 @@
+// app.js
 (function (){
+    var routerApp = angular.module('routerApp', ['ui.router']);
+
+    routerApp.config(function ($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/home');
+
+        // HOME STATES AND NESTED VIEWS ====================
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'partial-home.html'
+            })
+
+            // nested list with custom controller
+            .state('home.list', {
+               url: '/list',
+               templateUrl: 'partial-home-list.html',
+                controller: function ($scope){
+                   $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                }
+            })
+
+            // nested list with just some random string data
+            .state('home.paragraph', {
+                url: '/paragraph',
+                template: 'I could sure use a drink right now.'
+
+            })
+
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS ================
+            .state('about', {
+                // we'll get to this in a bit
+            })
+    });
 })();
