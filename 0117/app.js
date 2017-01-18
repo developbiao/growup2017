745a39670c52e5611ba973cb8e71c226ffa28a4c
gongbiao <developbiao@gmail.com>
2017-01-18 22:48:52 +0800

    Learning multiple views abount refrence scotch code.

--- 0117/app.js
+++ 0117/app.js
@@ -31,6 +31,50 @@
             // ABOUT PAGE AND MULTIPLE NAMED VIEWS ================
             .state('about', {
                 // we'll get to this in a bit
+                url: '/about',
+                views: {
+
+                    // the main template will be placed here (relatively named)
+                    '': { templateUrl: 'partial-about.html' },
+
+                    // the child views will be defined here (absolutely named)
+                    'columnOne@about': { template: 'Look i am a column' },
+
+                    // for column two, we'll define a separate controller
+                    'columnTwo@about' : {
+                        templateUrl: 'table-data.html',
+                        controller: 'scotchController'
+                    }
+                }
             })
+    }) // closes $routerApp.config();
+
+    // let's define the scotch controller that we call up in the about state
+    routerApp.controller('scotchController', function ($scope){
+
+        $scope.message = 'test';
+
+        $scope.scotches = [
+            {
+                name: 'Macallan 12',
+                price: 50
+
+            },
+            {
+                name: 'Chivas Regal Royal Salute',
+                price: 10000
+            },
+            {
+                name: 'Glenfiddich 1937',
+                price: 20000
+
+            },
+            {
+                name: 'Coca-cola',
+                price: 3
+
+            }
+        ];
     });
+
 })();
