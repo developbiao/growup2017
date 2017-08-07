d3954d1d5cd23293cc5aeacc257a45cc51aff15c
gongbiao <developbiao@gmail.com>
2017-08-07 23:25:31 +0800

    Learn angular animations.

--- 0725/component01/src/app/app.module.ts
+++ 0725/component01/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { MyNewComponentComponent } from './my-new-component/my-new-component.com
     MyNewComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 0725/component01/src/app/my-new-component/my-new-component.component.ts
+++ 0725/component01/src/app/my-new-component/my-new-component.component.ts
@@ -1,17 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-my-new-component',
   templateUrl: './my-new-component.component.html',
-  styleUrls: ['./my-new-component.component.css']
+  styleUrls: ['./my-new-component.component.css'],
+  animations: [
+      trigger('myAwesomeAnimation', [
+          state('small', style({
+              transform: 'scale(1)',
+          })),
+          state('large', style({
+              transform: 'scale(1.2)',
+          })),
+      ]),
+
+      transition('small=>large', animate('300ms ease-in')),
+  ]
 })
 export class MyNewComponentComponent implements OnInit {
 
-  constructor() { }
+    constructor(public dataService:DataService){
 
-  user_name = "Gong Biao";
+    }
 
-  ngOnInit() {
+    ngOnInit(){
+        console.log(this.dataService);
+    }
+
+    logoImage = "https://www.starbucks.com.cn/_themes/starbucks/img/mooncake2017/box1.png";
+    something = "What is a angular4?";
+    is_switch = false;
+
+
+  // array
+  myArr = ["Guitar", "Piano", "Harmonica"];
+  status = "true";
+
+  someProperty:string = this.dataService.myData();
+
+  changeImage(event)
+  {
+      if(this.is_switch)
+      {
+          this.logoImage = "https://www.starbucks.com.cn/_themes/starbucks/img/mooncake2017/box1.png";
+          this.is_switch = false;
+      }
+      else
+      {
+          this.logoImage = "http://www.starbucks.com.hk/media/BSM-550x420-02_tcm65-31096_w1024_n.png";
+          this.is_switch = true;
+      }
+      console.log("execute event...");
+  }
+
+  animateMe(){
+
   }
 
 }
